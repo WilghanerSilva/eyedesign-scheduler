c23fa971621ec700ce871adbdb7166a661abda24
Wilghaner Silva <wilghaner.silva@gmail.com>
2024-06-07 16:59:21 -0300

    First commit

---
+++ src/main/java/com/eyedesign/scheduler/EyedesignSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.eyedesign.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyedesignSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EyedesignSchedulerApplication.class, args);
+	}
+
+}
